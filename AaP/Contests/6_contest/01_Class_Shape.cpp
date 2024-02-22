b251e97a142d3c384a0a25e17694be0516088c1f
provod <42121873+Mobbub@users.noreply.github.com>
2024-02-23 01:12:24 +0300

    Update 01_Class_Shape.cpp

---
+++ AaP/Contests/6_contest/01_Class_Shape.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+class Shape {
+public:
+    virtual double getPerimeter() = 0;
+};
+
+class Rectangle : public Shape {
+private:
+    int length;
+    int width;
+public:
+    Rectangle(int a, int b) : length(a), width(b) {}
+    double getPerimeter() {
+        return 2 * (length + width);
+    }
+};
+
+class Circle : public Shape {
+private:
+    int radius;
+public:
+    Circle(int r) : radius(r) {}
+    double getPerimeter() {
+        return 2 * M_PI * radius;
+    }
+};
+
+class Triangle : public Shape {
+private:
+    int sideA;
+    int sideB;
+    int sideC;
+public:
+    Triangle(int a, int b, int c) : sideA(a), sideB(b), sideC(c) {}
+    double getPerimeter() {
+        if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA) {
+            return sideA + sideB + sideC;
+        } else {
+            return 0;
+        }
+    }
+};
