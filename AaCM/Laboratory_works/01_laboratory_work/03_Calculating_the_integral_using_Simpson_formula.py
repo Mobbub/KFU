0d27a0012a2b8fc660f36530452a5cd0aedec157
provod <42121873+Mobbub@users.noreply.github.com>
2024-03-30 23:42:24 +0300

    Update 03_Calculating_the_integral_using_Simpson_formula.py

--- AaCM/Laboratory_works/01_laboratory_work/03_Calculating_the_integral_using_Simpson_formula.py
+++ AaCM/Laboratory_works/01_laboratory_work/03_Calculating_the_integral_using_Simpson_formula.py
@@ -2,15 +2,12 @@ def f(x):
     return x ** 2
 
 def simp(a, b, n):
-    h = (b - a) / n
-    integ = f(a) + f(b)
-    for i in range(1, n, 2):
-        integ += 4 * f(a + i * h)
-    for j in range(1, n - 1, 2):
-        integ += 2 * f(a + j * h)
-    integ *= h / 3
-    return integ
-
+    h = float(b - a) / n
+    integ = abs(func(a)) + abs(func(b))
+    for i in range(1, n):
+        integ += abs(func(a + i * h)) * (2 if i % 2 == 0 else 4)
+    return integ * h / 3
+    
 a = 0
 b = 1
 n = 100
