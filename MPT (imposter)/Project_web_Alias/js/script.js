2486efc8298de3392f3706edba6f1be3acf3d61b
promob <42121873+Mobbub@users.noreply.github.com>
2024-06-23 23:20:40 +0300

    Delete MPT (imposter)/Project_web_Alias directory

--- MPT (imposter)/Project_web_Alias/js/script.js
+++
@@ -1,9 +0,0 @@
-var rules_element = document.querySelector('.rules-wrapper');
-
-document.getElementById("rules").onclick = function() {
-    rules_element.classList.toggle('rules-active')
-};
-
-document.getElementById("rules-close").onclick = function() {
-    rules_element.classList.toggle('rules-active')
-};
